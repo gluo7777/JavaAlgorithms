987da2a1b90de523e2ba487951a473bccb017aeb
William Luo <gluo7777@gmail.com>
2017-07-24 16:26:28 -0500

    LeetCode easy 2

---
+++ ArrayPairSum.java
@@ -0,0 +1,27 @@
+package org.william.leetcode;
+
+import java.util.Arrays;
+
+public class ArrayPairSum {
+	
+	/**
+	 * Group nums in such a way that Sum(min(Ai,Bi)) is maximized 
+	 * Strategy:
+	 * 1) Sort array first
+	 * 2) Rationale: Sorted array ensures that  the second largest element will always be included in the sum
+	 * 3) Add to result
+	 * @param nums
+	 * @return
+	 */
+	public static int arrayPairSum(int[] nums) {
+		int result = 0;
+		
+		Arrays.sort(nums);
+		
+		for(int i=0 ; i<=nums.length-2 ; i+=2){
+			result += Math.min(nums[i], nums[i+1]);
+		}
+		
+		return result;
+	}
+}
